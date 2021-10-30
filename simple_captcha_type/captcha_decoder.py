77ca57e4f3fbc27b6c069eb265ab23c59b74d812
samorojy <87e7L@vK>
2021-10-30 16:22:55 +0300

    update style

--- simple_captcha_type/captcha_decoder.py
+++ simple_captcha_type/captcha_decoder.py
@@ -12,24 +12,26 @@ def decoder(image_file_name):
 
 
 def detekt_letter(image):
-    alphabet_list_with_weight = [('A', 230), ('B', 270), ('C', 190), ('D', 282), ('E', 208), ('F', 166), ('G', 224),
-                                 ('H', 260),
-                                 ('I', 112), ('J', 131), ('K', 228), ('L', 148), ('M', 368), ('N', 268), ('O', 265),
-                                 ('P', 211),
-                                 ('Q', 297), ('R', 263), ('S', 185), ('T', 172), ('U', 217), ('V', 197), ('W', 354),
-                                 ('X', 216),
+    alphabet_list_with_weight = [('A', 230), ('B', 270), ('C', 190), ('D', 282), ('E', 208),
+                                 ('F', 166), ('G', 224), ('H', 260),
+                                 ('I', 112), ('J', 131), ('K', 228), ('L', 148), ('M', 368),
+                                 ('N', 268), ('O', 265), ('P', 211),
+                                 ('Q', 297), ('R', 263), ('S', 185), ('T', 172), ('U', 217),
+                                 ('V', 197), ('W', 354), ('X', 216),
                                  ('Y', 165), ('Z', 211)]
     pixel_data = image.load()
     letter_similarity_list = []
     for letter in alphabet_list_with_weight:
         letter_image_pixel_data = Image.open(f"alphabet/{letter[0]}.png").load()
         letter_similarity_counter = 0
-        for y in range(image.size[1]):
-            for x in range(image.size[0]):
-                if letter_image_pixel_data[x, y] == (0, 0, 0) and pixel_data[x, y] != (0, 0, 0):
+        for y_coordinate in range(image.size[1]):
+            for x_coordinate in range(image.size[0]):
+                if letter_image_pixel_data[x_coordinate, y_coordinate] == (0, 0, 0) \
+                        and pixel_data[x_coordinate, y_coordinate] != (0, 0, 0):
                     letter_similarity_counter = -1
                     break
-                if letter_image_pixel_data[x, y] == (0, 0, 0) and pixel_data[x, y] == (0, 0, 0):
+                if letter_image_pixel_data[x_coordinate, y_coordinate] == (0, 0, 0) \
+                        and pixel_data[x_coordinate, y_coordinate] == (0, 0, 0):
                     letter_similarity_counter += 1
             if letter_similarity_counter == -1:
                 break
--- simple_captcha_type/captcha_letters_cutter.py
+++ simple_captcha_type/captcha_letters_cutter.py
@@ -16,12 +16,12 @@ def cut_letters(image_file_name):
 # Clean the background noise, if color != white, then set to black.
 def clear_noise(image, threshold=200):
     pixdata = image.load()
-    for y in range(image.size[1]):
-        for x in range(image.size[0]):
-            if (pixdata[x, y][0] > threshold) \
-                    and (pixdata[x, y][1] > threshold) \
-                    and (pixdata[x, y][2] > threshold):
+    for y_coordinate in range(image.size[1]):
+        for x_coordinate in range(image.size[0]):
+            if (pixdata[x_coordinate, y_coordinate][0] > threshold) \
+                    and (pixdata[x_coordinate, y_coordinate][1] > threshold) \
+                    and (pixdata[x_coordinate, y_coordinate][2] > threshold):
 
-                pixdata[x, y] = (255, 255, 255)
+                pixdata[x_coordinate, y_coordinate] = (255, 255, 255)
             else:
-                pixdata[x, y] = (0, 0, 0, 255)
+                pixdata[x_coordinate, y_coordinate] = (0, 0, 0, 255)
