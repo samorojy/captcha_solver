37fe2bdbc1e09b80945bbce6e6b8140f8171b0a6
samorojy <87e7L@vK>
2021-11-01 18:47:31 +0300

    update style

--- simple_captcha_type/captcha_decoder2.py
+++ simple_captcha_type/captcha_decoder2.py
@@ -11,7 +11,7 @@ def decoder(image_file_name):
     captcha_letters_cutter.clear_noise(image)
     answer = ""
     letter_width = 0
-    for x in range(0, 4):
+    for _ in range(0, 4):
         box = (letter_width, 0, 150, 40)
         detekt_result = detekt_letter(image.crop(box))
         letter_width = letter_width + detekt_result[2]
@@ -20,12 +20,12 @@ def decoder(image_file_name):
 
 
 def detekt_letter(image):
-    alphabet_list_with_weight = [('A', 230, 26), ('B', 270, 22), ('C', 190, 26), ('D', 282, 28), ('E', 208, 21),
-                                 ('F', 166, 20), ('G', 224, 27), ('H', 260, 28),
-                                 ('I', 112, 11), ('J', 131, 12), ('K', 228, 25), ('L', 148, 20), ('M', 368, 33),
-                                 ('N', 268, 28), ('O', 265, 30), ('P', 211, 21),
-                                 ('Q', 297, 30), ('R', 263, 24), ('S', 185, 20), ('T', 172, 24), ('U', 217, 26),
-                                 ('V', 197, 25), ('W', 354, 33), ('X', 216, 24),
+    alphabet_list_with_weight = [('A', 230, 26), ('B', 270, 22), ('C', 190, 26), ('D', 282, 28),
+                                 ('E', 208, 21),('F', 166, 20), ('G', 224, 27), ('H', 260, 28),
+                                 ('I', 112, 11), ('J', 131, 12), ('K', 228, 25), ('L', 148, 20),
+                                 ('M', 368, 33),  ('N', 268, 28), ('O', 265, 30), ('P', 211, 21),
+                                 ('Q', 297, 30), ('R', 263, 24), ('S', 185, 20), ('T', 172, 24),
+                                 ('U', 217, 26),   ('V', 197, 25), ('W', 354, 33), ('X', 216, 24),
                                  ('Y', 165, 24), ('Z', 211, 23)]
     pixel_data = image.load()
     letter_similarity_list = []
@@ -53,9 +53,9 @@ def detekt_letter(image):
 
 
 if __name__ == '__main__':
-    dataset_directory = "incorrect_captcha"
-    for filename in os.listdir(dataset_directory):
+    DATASET_DIRECTORY = "incorrect_captcha"
+    for filename in os.listdir(DATASET_DIRECTORY):
         captcha_solve = filename[:-3]
         image_filename = captcha_solve + "png"
-        decode_result = decoder(os.path.join(dataset_directory, image_filename))
+        decode_result = decoder(os.path.join(DATASET_DIRECTORY, image_filename))
         print(f"{decode_result}:{captcha_solve[:-1]} {decode_result == captcha_solve[:-1]}")
